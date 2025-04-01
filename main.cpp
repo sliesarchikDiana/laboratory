048971b616d99042ce54fa57a7ea62a8f8eb0975
Diana <slesarcukdiana8@gmail.com>
2025-04-01 18:48:05 +0300

    make class thriller derived

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 #include "action.h"
 #include "sciFy.h"
 #include "romance.h"
-//#include <thriller.h>
+#include <thriller.h>
 //#include <utility>
 using namespace std;
 
 int main() {
     cout << "Test begin"<< endl;
 
-/*    action avengers("Avengers: Endgame", "Brothers Russo", "Superheroes save the planet", 181, 356000000, 20);
+    action avengers("Avengers: Endgame", "Brothers Russo", "Superheroes save the planet", 181, 356000000, 20);
     action defaultAction;
 
     std::cout << std::endl;
@@ -28,7 +28,7 @@ int main() {
     alien.displayDetails();
     std::cout << std::endl;
     defaultSciFy.displayDetails();
-*/
+
     std::cout<<std::endl << "Movies count: " << sciFy::getObjectCount() << std::endl;
     std::cout << std::endl;
     romance sweetHomeAlabama("Sweet Home Alabama", "Andy Tennant", "A woman must choose between two loves.", 108, 30000000, true);
@@ -47,35 +47,44 @@ int main() {
     std::cout<<std::endl << "Movies count: " << action::getObjectCount() << std::endl;
     std::cout << std::endl;
 
-
-/*    thriller silenceOfLambs("The Silence of the Lambs", "Jonathan Demme","FBI cadet must receive help from a cannibal to catch another serial killer.", 118, 19000000);
+    thriller silenceOfLambs("The Silence of the Lambs", "Jonathan Demme","FBI cadet must receive help from a cannibal to catch another serial killer.", 118, 19000000, false);
     thriller defaultThriller;
-
-    auto* originalThriller = new thriller("Inception", "Christopher Nolan", "A mind-bending heist within dreams", 148, 160000000);
+    thriller frankenweenie("Frankenweenie","Tim Burton","A boy revives his dog in a science experiment, leading to unexpected consequences.",87,39,true);
+    auto* originalThriller = new thriller("Inception", "Christopher Nolan", "A mind-bending heist within dreams", 148, 160000000, false);
     thriller movedThriller(std::move(*originalThriller));
     delete originalThriller;
-*/
     std::cout << std::endl;
+
+
+    std::cout << std::endl;
+    silenceOfLambs.displayDetails();
+    std::cout << std::endl;
+    defaultThriller.displayDetails();
+    std::cout << std::endl;
+    frankenweenie.displayDetails();
+    std::cout << std::endl;
+    movedThriller.displayDetails();
+
 /*    std::cout << "Moved Thriller Title: " << movedThriller.getTitle() << std::endl;
     std::cout << std::endl;
-
-    const thriller se7en("Se7en", "David Fincher", "Detectives track down a serial killer who uses deadly sins as motives.", 127, 33000000);
-    std::cout << "Title: " << se7en.getTitle() << ", Director: " << se7en.getDirector()<<std::endl<< "Plot: " << se7en.getPlot()<<", Runtime: " << se7en.getRunningTime() << " minutes"<< ", Budget: " << se7en.getBudget() << std::endl;
+*/
+    const thriller se7en("Se7en", "David Fincher", "Detectives track down a serial killer who uses deadly sins as motives.", 127, 33000000, false);
     std::cout << std::endl;
 
-    std::cout<<silenceOfLambs;
+    std::cout << std::endl;
+    se7en.displayDetails();
     std::cout << std::endl;
 
-    std::cout << "Object count for thriller: " << thriller::getObjectCount() << std::endl;
+    std::cout<<std::endl << "Movies count: " << action::getObjectCount() << std::endl;
     std::cout << std::endl;
 
-    thriller myThriller;
+/*    thriller myThriller;
     std::cin >> myThriller;
     std::cout << myThriller;
 
     int totalRuntime = silenceOfLambs + movedThriller;
     std::cout << "Total Running Time: " << totalRuntime << " minutes" << std::endl;
-    */
     std::cout << std::endl;
+    */
     return 0;
 }
--- thriller.cpp
+++ thriller.cpp
@@ -1,66 +1,59 @@
 #include "thriller.h"
+#include "movie.h"
 #include <iostream>
 #include <string>
 #include <utility>
-int thriller::count = 0;
 
-thriller::thriller(): title("Unknown thriller"), director("Unknown"), plot("Unknown"), runningTime(0), budget(0) {
-    ++count;
-    std::cout << "Default constructor was called for " << this->title<<" of class thriller" << std::endl;
-}
-thriller::thriller(std::string t, std::string d, std::string p, int rt, int b): title(std::move(t)), director(std::move(d)), plot(std::move(p)), runningTime(rt), budget(b) {
-    ++count;
-    std::cout << "Parameterized constructor was called for " << this->title<<" of class thriller" << std::endl;
+thriller::thriller() : movie("Unknown thriller", "Unknown", "Unknown", 0, 0), isChildFriendly(false) {
+    std::cout << "Default constructor was called for " << getTitle() << " of class thriller" << std::endl;
 }
 
-// Copy constructor
-thriller::thriller(const thriller& other) = default;
-// Destructor
-thriller::~thriller() {
-    std::cout << "The destructor was called for " << title <<" of class thriller" << std::endl;
-    --count;
+thriller::thriller(std::string title, std::string director, std::string plot, int runningTime, int budget, bool childFriendly)
+    : movie(std::move(title), std::move(director), std::move(plot), runningTime, budget), isChildFriendly(childFriendly) {
+    std::cout << "Parameterized constructor was called for " << getTitle() << " of class thriller" << std::endl;
 }
 
-
-std::string thriller::getTitle() const { return this->title; }
-std::string thriller::getDirector() const { return this->director; }
-std::string thriller::getPlot() const { return this->plot; }
-int thriller::getRunningTime() const { return this->runningTime; }
-int thriller::getBudget() const { return this->budget; }
-
-
-void thriller::setTitle(const std::string& newTitle) { this->title = newTitle; }
-void thriller::setDirector(const std::string& newDirector) { this->director = newDirector; }
-void thriller::setPlot(const std::string& newPlot) { this->plot = newPlot; }
-void thriller::setRunningTime(int newRunningTime) {
-    if (newRunningTime > 0) {
-        this->runningTime = newRunningTime;
-    }
-}
-void thriller::setBudget(int newBudget) {
-    if (newBudget >= 0) {
-        this->budget = newBudget;
-    }
+thriller::thriller(const thriller& other)
+    : movie(other), isChildFriendly(other.isChildFriendly) {
+    std::cout << "Copy constructor was called for " << getTitle() << " of class thriller" << std::endl;
 }
 
 thriller::thriller(thriller&& other) noexcept
-    : title(std::move(other.title)),
-      director(std::move(other.director)),
-      plot(std::move(other.plot)),
-      runningTime(other.runningTime),
-      budget(other.budget) {
-     ++count;
-    std::cout << "Move constructor was called for " << this->title <<" of class thriller" << std::endl;
-    other.title = "Discarded";
-    other.director = "Unknown";
-    other.plot = "Unknown";
-    other.runningTime = 0;
-    other.budget = 0;
+    : movie(std::move(other)){
+    std::cout << "Move constructor was called for " << getTitle() << " of class thriller" << std::endl;
 }
-int thriller::getObjectCount() { return count; }
+
+thriller::~thriller() {
+    std::cout << "The destructor was called for " << getTitle() << " of class thriller" << std::endl;
+}
 
 
-std::ostream& operator<<(std::ostream& os, const thriller& t) {
+int thriller::getIsChildFriendly() const {
+    return isChildFriendly;
+}
+void thriller::setIsChildFriendly(bool newIsChildFriendly) {
+    isChildFriendly = newIsChildFriendly;
+}
+
+std::string thriller::isChildFriendlyToMessage(bool childFriendly) {
+    if (childFriendly) {
+        return "is child friendly";
+    } else {
+        return "for adult audience only";
+    }
+}
+
+void thriller::displayDetails() const {
+    std::cout << "Action Movie: " << getTitle()
+              << "\nDirector: " << getDirector()
+              << "\nPlot: " << getPlot()
+              << "\nRunning Time: " << getRunningTime() << " minutes"
+              << "\nBudget: " << getBudget() <<"$"
+              << "\nAge limit: " << isChildFriendlyToMessage(isChildFriendly) << std::endl;
+}
+
+/*
+ std::ostream& operator<<(std::ostream& os, const thriller& t) {
     os << "Title: " << t.title << "\n"
        << "Director: " << t.director << "\n"
        << "Plot: " << t.plot << "\n"
@@ -86,5 +79,4 @@ std::istream& operator>>(std::istream& is, thriller& t) {
 int operator+(const thriller& lhs, const thriller& rhs) {
     return lhs.getRunningTime() + rhs.getRunningTime();
 }
-
-
+*/
